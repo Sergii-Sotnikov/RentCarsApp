67e16f77ad631e10757fb754c9938853302d769c
SotnikovSS <sotnikovss90@gmail.com>
2025-11-27 23:45:33 +0100

    go

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
 import css from "./Header.module.css";
-import TagsMenu from "../TagsMenu/TagsMenu";
-
+import Image from "next/image";
+import logo from "@/app/public/img/header/logo.png";
 
 const Header = () => {
   return (
-<header className={css.header}>
-    <Link href="/" aria-label="Home">NoteHub</Link>
-  <nav aria-label="Main Navigation">
-    <ul className={css.navigation}>
-      <li>
-        <Link href="/">Home</Link>
-        <TagsMenu/>
-      </li>
-    </ul>
-  </nav>
-</header>
+    <header className={css.header}>
+      <div className={css.container}>
+        <Link className={css.linkLogo} href="/">
+          <Image src={logo} className={css.logo} alt="Logo" />
+        </Link>
+        <nav className={css.nav}>
+          <Link href="/" className={css.navElem}>
+            Home
+          </Link>
+          <Link href="/catalog" className={css.navElem}>
+            Catalog
+          </Link>
+        </nav>
+      </div>
+    </header>
   );
 };
 
